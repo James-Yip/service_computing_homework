f4f6be8a9873ccc48b6763b26ade8a6c5848ed5c
James <874244887@qq.com>
2017-10-18 17:43:04 +0800

    selpg_v2

--- selpg/README.md
+++ selpg/README.md
@@ -1,8 +1,167 @@
 # selpg
+selpg is a useful program that extracts a specified range of pages from an input text file.
+Here, I implement it by Golang, referring to [开发Linux命令行实用程序](https://www.ibm.com/developerworks/cn/linux/shell/clutil/index.html).
 
-## 设计说明
-该程序的具体实现主要
+## Usage
+> Usage of selpg: ./selpg -s number -e number [-f | -l number] [-d string] [in_filename]
+  -s int
+      the start page number to extract an input text(mandatory) (default -1)
+  -e int
+    	the end page number to extract an input text(mandatory) (default -1)
+  -d string
+      the destination to output the selected pages
+  -f bool
+      use form feed to define the pages of an input text file (default false)
+  -l int
+    	#lines of each page (default 72)
+  in_filename string
+      the name of the input file
 
-## 使用
+## Design Introduction
+The selpg program can be divided into three parts:
+1. parse the input command-line (implemented in function parse_input)
+2. check the validity of the input arguments (implemented in function check_input)
+3. process the intput text and extract a specified range of pages (implemented in function process_input)
+As shown in the flowchart:
+![selpg_flowchart](./selpg_flowchart.png)
 
-## 测试结果
+## Packages
+To implement this program, I use some handy packages from std-lib:
+- flag:    use to parse and obtain the arguments from command-line;
+- bufio:   use to read the input text conveniently;
+- os/exec: use to execute an extra command if the -f arguments is given.
+
+For the detail implementation, please see the source code.
+(It will be easy to read and understand, maybe)
+
+## Examples
+Note: the file used to test is a type2 input text file (but we also use it to test the type1 case):
+```
+$ cat input
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+line 5 of input file
+line 6 of input file
+line 7 of input file
+line 8 of input file
+```
+
+1. Input from file:
+```
+$ ./selpg -s 1 -e 2 -l 2 input
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+selpg: done
+```
+
+2. Input from stdin:
+```
+$ ./selpg -s 1 -e 2 -l 2 < input
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+selpg: done
+```
+
+3. Input from the output of other command:
+```
+$ cat input | ./selpg -s 1 -e 2 -l 2
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+selpg: done
+```
+
+4. Save the output into file:
+```
+$ ./selpg -s 1 -e 2 -l 2 input > output
+selpg: done
+```
+In the output file:
+```
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+```
+
+5. Save the error message into file:
+```
+$ ./selpg -s 3 -e 2 -l 2 input 2> error
+```
+In the error file:
+```
+selpg: The end page should be greater than the start page.
+Usage of ./selpg:
+  -d string
+    	the destination to output the selected pages
+  -e int
+    	the end page number to extract an input text(mandatory) (default -1)
+  -f	use form feed to define the pages of an input text
+  -l int
+    	#lines of each page (default 72)
+  -s int
+    	the start page number to extract an input text(mandatory) (default -1)
+exit status 1
+```
+
+6. type2 case(-f is given):
+```
+$ ./selpg -s 1 -e 2 -f input
+line 1 of input file
+line 2 of input file
+line 3 of input file
+line 4 of input file
+selpg: done
+```
+
+7. excute extra command(its input is the output of selpg):
+```
+$ ./selpg -s 1 -e 2 -l 2 -d "sort -r" input
+selpg: done
+line 4 of input file
+line 3 of input file
+line 2 of input file
+line 1 of input file
+
+```
+
+8. Some invalid input cases:
+```
+$ ./selpg -s 1 -e 2 -l 2 -f input
+selpg: Flags -l & -f are mutually exclusive.
+Usage of /tmp/go-build700220200/command-line-arguments/_obj/exe/selpg:
+  -d string
+    	the destination to output the selected pages
+  -e int
+    	the end page number to extract an input text(mandatory) (default -1)
+  -f	use form feed to define the pages of an input text
+  -l int
+    	#lines of each page (default 72)
+  -s int
+    	the start page number to extract an input text(mandatory) (default -1)
+exit status 1
+```
+
+```
+$ ./selpg -s -1 -e 2 input
+selpg: The start page / end page should be positive integer.
+Usage of /tmp/go-build700220200/command-line-arguments/_obj/exe/selpg:
+  -d string
+    	the destination to output the selected pages
+  -e int
+    	the end page number to extract an input text(mandatory) (default -1)
+  -f	use form feed to define the pages of an input text
+  -l int
+    	#lines of each page (default 72)
+  -s int
+    	the start page number to extract an input text(mandatory) (default -1)
+exit status 1
+```
+...
--- selpg/selpg.go
+++ selpg/selpg.go
@@ -1,11 +1,25 @@
+/*=================================================================
+
+Program name:
+	selpg (SELect PaGes implemented by Golang)
+
+Purpose:
+	Extract a specified range of pages from an input text file.
+
+===================================================================*/
+
 package main
 
 import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 )
+
+/*================================ types =========================*/
 
 type sp_flags struct {
 	start_page  int
@@ -16,60 +30,102 @@ type sp_flags struct {
 	print_dest  string
 }
 
+/*=============================== globals =========================*/
+
+var progname string = "selpg" // program name, for error messages
+
+/*================================ main ===========================*/
+
 func main() {
 	var sf sp_flags
-	// parse the input flags and use them to initialize struct sf
-	parse_flags(&sf)
-	// check the validation of the input flags
-	check_validation(sf)
+	parse_input(&sf)
+	check_input(sf)
 	process_input(sf)
 }
 
-func parse_flags(sf *sp_flags) {
+/*============================ parse_input ========================*/
+// parse the input flags & arguments
+// and use them to initialize struct sf
+func parse_input(sf *sp_flags) {
 	flag.IntVar(&sf.start_page, "s", -1, "the start page number to extract an input text(mandatory)")
 	flag.IntVar(&sf.end_page, "e", -1, "the end page number to extract an input text(mandatory)")
 	flag.IntVar(&sf.page_len, "l", 72, "#lines of each page")
 	flag.BoolVar(&sf.useFormFeed, "f", false, "use form feed to define the pages of an input text")
-	flag.StringVar(&sf.print_dest, "d", "", "the destination to print the select pages")
+	flag.StringVar(&sf.print_dest, "d", "", "the destination to output the selected pages")
 	flag.Parse()
 	sf.in_filename = flag.Arg(0)
-	fmt.Printf("The size of args is: %d\n", flag.NArg())
 }
 
-func check_validation(sf sp_flags) {
+/*============================ check_input ========================*/
+// check the validity of the input flags
+func check_input(sf sp_flags) {
 	errMessage := ""
 	switch {
-	case flag.NFlag() < 2:
-		errMessage = "not enough arguments"
+	case flag.NArg() > 1:
+		errMessage = "Only support one input text file."
 	case sf.start_page < 1 || sf.end_page < 1:
-		errMessage = "The start_page/end_page should be positive integer."
+		errMessage = "The start page / end page should be positive integer."
 	case sf.start_page > sf.end_page:
-		errMessage = "The start_page can't larger than the end_page."
+		errMessage = "The end page should be greater than the start page."
+	case sf.page_len < 1:
+		errMessage = "Page length should be positive integer."
 	case sf.useFormFeed && sf.page_len != 72:
-		errMessage = "-l & -f is mutually exclusive."
+		errMessage = "Flags -l & -f are mutually exclusive."
 	}
 	if errMessage != "" {
-		fmt.Printf("%s\n", errMessage)
+		fmt.Fprintf(os.Stderr, "%s: %s\n", progname, errMessage)
 		flag.Usage()
 		os.Exit(1)
 	}
 }
 
+/*========================== process_input ========================*/
+
 func process_input(sf sp_flags) {
+	// declaration
+	var fin io.ReadCloser      // input stream
+	var fout io.WriteCloser    // output stream
+	var line_ctr int           // line counter
+	var page_ctr int           // page counter
+	var err error              // error message
+	var cmd *exec.Cmd          // execute extra command if flag -d is given
+	var scanner *bufio.Scanner // scanner used to scan the input stream
+
 	// set the input source
-	fin, err := os.Open(sf.in_filename)
 	if sf.in_filename == "" {
 		fin = os.Stdin
-	} else if fin == nil {
-		panic(err)
-		os.Exit(2)
+	} else {
+		fin, err = os.Open(sf.in_filename)
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr,
+				"%s: fail to open %s(%s)\n",
+				progname, sf.in_filename, err)
+			os.Exit(2)
+		}
 	}
 
-	line_ctr := 0
-	page_ctr := 1
+	// set the output source
+	if sf.print_dest == "" {
+		fout = os.Stdout
+	} else {
+		cmd = exec.Command("sh", "-c", sf.print_dest)
+		fout, err = cmd.StdinPipe()
+		if fout == nil {
+			fmt.Fprintf(
+				os.Stderr,
+				"%s: fail to open pipe(%s)\n",
+				progname, sf.in_filename, err)
+			os.Exit(3)
+		}
+	}
+
 	// start processing
-	scanner := bufio.NewScanner(fin)
+	scanner = bufio.NewScanner(fin)
 	if !sf.useFormFeed {
+		line_ctr = 0
+		page_ctr = 1
+		// process type1 input text
 		for scanner.Scan() {
 			line_ctr++
 			if line_ctr > sf.page_len {
@@ -77,16 +133,22 @@ func process_input(sf sp_flags) {
 				line_ctr = 1
 			}
 			if (page_ctr >= sf.start_page) && (page_ctr <= sf.end_page) {
-				fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
+				fmt.Fprintf(fout, "%s\n", scanner.Text())
 			}
 		}
 	} else {
-		for _, c := range scanner.Text() {
-			if c == '\f' {
-				page_ctr++
+		page_ctr = 1
+		// process type2 input text
+		for scanner.Scan() {
+			for _, c := range scanner.Text() {
+				if c == '\f' {
+					page_ctr++
+				} else if (page_ctr >= sf.start_page) && (page_ctr <= sf.end_page) {
+					fmt.Fprintf(fout, "%s", string(c))
+				}
 			}
 			if (page_ctr >= sf.start_page) && (page_ctr <= sf.end_page) {
-				fmt.Fprintf(os.Stdout, "%s\n", string(c))
+				fmt.Fprintf(fout, "\n")
 			}
 		}
 	}
@@ -94,13 +156,28 @@ func process_input(sf sp_flags) {
 
 	if page_ctr < sf.start_page {
 		// output error message
-		fmt.Fprintf(os.Stderr, "start_page (%d) larger than total pages (%d), no output written\n", sf.start_page, page_ctr)
+		fmt.Fprintf(
+			os.Stderr,
+			"%s: start_page(%d) greater than total pages (%d), no output written\n",
+			progname, sf.start_page, page_ctr)
 	} else if page_ctr < sf.end_page {
 		// output error message
-		fmt.Fprintf(os.Stderr, "end_page (%d) larger than total pages (%d), less output than expected\n", sf.end_page, page_ctr)
+		fmt.Fprintf(
+			os.Stderr,
+			"%s: end_page(%d) greater than total pages (%d), less output than expected\n",
+			progname, sf.end_page, page_ctr)
 	}
-	if fin != nil {
-		fin.Close()
+
+	// execute extra command if flag -d is given
+	if sf.print_dest != "" {
+		cmd.Stdout = os.Stdout
+		err := cmd.Start()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: print fails\n", progname)
+		}
 	}
-	fmt.Printf("selpg: done\n")
+
+	fin.Close()
+	fout.Close()
+	fmt.Fprintf(os.Stderr, "%s: done\n", progname)
 }
